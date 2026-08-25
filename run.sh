aa017af3ebe6eabfb010e7810e597ec5c7b4d557
trankhanhduy2929-beep <trankhanhduy2929@gmail.com>
2026-08-25 19:56:23 +0700

    chore(repo): delete 4 item(s)

--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/with-contenv bashio
-exec gunicorn --bind 0.0.0.0:2032 --worker-class gthread --workers 1 --threads 8 --timeout 0 --keep-alive 10 --access-logfile - --error-logfile - app:app
